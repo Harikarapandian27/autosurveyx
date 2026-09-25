03f5b48267b73685e88a67ca4a2fc35566897d1c
Harikarapandian27 <harikarapandian@gmail.com>
2026-09-25 14:20:55 +0530

    Update admin.js

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const db = require('../models/db');
+const db = require('../db');
 const { requireAdmin } = require('../middleware/auth');
 const config = require('../config');
 
